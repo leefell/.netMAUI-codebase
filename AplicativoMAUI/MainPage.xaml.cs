228a00de0123db52499bc5992b8310e8a5bfd71c
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-13 19:24:16 -0300

    Merge buttons

--- AplicativoMAUI/MainPage.xaml.cs
+++ AplicativoMAUI/MainPage.xaml.cs
@@ -2,22 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-        string nomeUsuario, idadeUsuario;
         public MainPage()
         {
             InitializeComponent();
         }
         private async void BTNOla_Clicked(object sender, EventArgs e)
         {
-            nomeUsuario = await DisplayPromptAsync("Nome", "Digite seu nome:", "Ok");
+            string nomeUsuario = await DisplayPromptAsync("Nome", "Digite seu nome:", "Ok");
+            string idadeUsuario = await DisplayPromptAsync("Sua Idade", "Digite sua idade: ", "Ok");
             await DisplayAlert("ALERTA", "Olá " + nomeUsuario + "!" +
-                "\nVocê está vendo essa notificação às: " + DateTime.Now.ToString(), "Fechar");
-        }
-
-        private async void BTNIdade_Clicked(object sender, EventArgs e)
-        {
-            idadeUsuario = await DisplayPromptAsync("Sua Idade", "Digite sua idade: ", "Ok");
-            await DisplayAlert("Saudações", "Você tem: " + idadeUsuario + " anos.", "Fechar");
+                "\nVocê tem " + idadeUsuario + " anos e " +
+                "está vendo essa notificação às: \n" + DateTime.Now.ToString(), "Fechar");
         }
     }
 }
