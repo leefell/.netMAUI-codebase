be6fd19d35e7843a6cc9fc0fbf2157dd5f775c62
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-13 16:31:58 -0300

    Testando tags e botões

---
+++ AplicativoMAUI/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace AplicativoMAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BTNOla_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("ALERTA", "Olá Mundo! \nVocê está vendo essa notificação!", "Fechar");
+        }
+
+        private void BTNBrincadeira_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Horário Atual: ", DateTime.Now.ToString(), "Fechar");
+        }
+    }
+}
