0df4d493260fe4787f88a41796175366d1f9bada
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-16 21:25:04 -0300

    Upload do icone

--- ProjetosMAUI/LuckyNumberApp/MauiProgram.cs
+++ ProjetosMAUI/LuckyNumberApp/MauiProgram.cs
@@ -16,7 +16,7 @@ namespace LuckyNumberApp
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
