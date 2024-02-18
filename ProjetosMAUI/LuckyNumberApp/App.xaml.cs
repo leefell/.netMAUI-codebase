90fdbbb6b4cd83e461c7fd0ee3877cabfcdef8bf
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-17 21:57:58 -0300

    SplashScreen

--- ProjetosMAUI/LuckyNumberApp/App.xaml.cs
+++ ProjetosMAUI/LuckyNumberApp/App.xaml.cs
@@ -6,7 +6,8 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //CurrentPage = Página atual
+            MainPage = new MainPage();
         }
     }
 }
