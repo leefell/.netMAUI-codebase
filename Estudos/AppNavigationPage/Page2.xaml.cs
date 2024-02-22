d6b2c694e433d4cd9a928057b8e7ff9a12a33962
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-22 11:15:19 -0300

    Fluxo de navegação

--- Estudos/AppNavigationPage/Page2.xaml.cs
+++ Estudos/AppNavigationPage/Page2.xaml.cs
@@ -6,4 +6,21 @@ public partial class Page2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnButtonNextClicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new Page3());
+    }
+
+    private void OnButtonPreviousClicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync(); // volta a pagina anterior, mas deve ter certeza que existe uma pagina anterior
+        // Navigation.NavigationStack é o empilhamento das paginas
+        // Navigation.ModalStack a ideia do modal é que ele nao usa a barra de titulo, ela é apresentada sem o botao de tirar padrao
+        // tipo termos de uso durante a instalacao de algo
+
+        // Navigation.PopToRootAsync(); volta direto ao inicio do fluxo (exemplo um cadastro de etapas)
+        // Navigation.RemovePage(Navigation.NavigationStack[1]); // removeria a pagina 2 da pilha
+        //Navigation.InsertPageBefore(2, 3); // adicionar uma nova pagina dentro da pilha
+    }
 }
--- Estudos/AppNavigationPage/Page3.xaml.cs
+++ Estudos/AppNavigationPage/Page3.xaml.cs
@@ -6,4 +6,9 @@ public partial class Page3 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PopAsync();
+    }
 }
