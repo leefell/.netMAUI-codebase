4dba923e996065a84b8e5b3779da2400aed92c9e
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-02-25 11:51:26 -0300

    Criando Models

---
+++ ProjetosMAUI/AppMAUIGallery/README.md
@@ -0,0 +1,3 @@
+# MAUI GAllery APP
+
+O Objetivo desse aplicativo além de estudar o framework é reunir e apresentar alguns componentes presentes no .NET MAUI.
---
+++ ProjetosMAUI/AppMAUIGallery/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Layouts;
+
+namespace AppMAUIGallery.Repositories
+{
+    // essa classe vai funcionar tipo um banco de dados
+    internal class CategoryRepository
+    {
+        public CategoryRepository()
+        {
+
+        }
+        public List<Category> GetCategories()
+        {
+            // Foi criado uma categoria e um componente
+
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category
+            {
+                Name = "Layout",
+                Components = new List<Component> {
+                    new Component {
+                    Title = "StackLayout",
+                    Description = "Organização sequencial dos elementos.",
+                    Page = new StackLayoutPage()} }
+            });
+
+            return categories;
+        }
+    }
+}
