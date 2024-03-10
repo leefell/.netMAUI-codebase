1f85156b35e3e53be0f65c0242897e5e51567c40
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-03-10 10:54:33 -0300

    Image

---
+++ ProjetosMAUI/AppMAUIGallery/Views/Components/Mains/ImagePage.xaml.cs
@@ -0,0 +1,17 @@
+namespace AppMAUIGallery.Views.Components.Mains;
+
+public partial class ImagePage : ContentPage
+{
+	public ImagePage()
+	{
+		InitializeComponent();
+
+		/*
+		 * Imagem01.Source pode ter 4 valores:
+		 * Image.FromFile
+		 * Image.FromResource
+		 * Image.FromStream
+		 * Image.FromUri
+		 */
+	}
+}
